06c18a6d22578fb167b4c7282a2add6cb29fb280
Nik Bornemeier <NikolausRB@gmail.com>
2018-11-26 11:31:48 -0700

    add routing for Interiors and Exteriors Components

---
+++ src/Components/Exteriors/Exteriors.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Exteriors.css';
+
+const Exteriors = () => {
+  return (
+    <h2>Exteriors</h2>
+  )
+};
+
+export default Exteriors;
---
+++ src/Components/Interiors/Interiors.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Interiors.css';
+
+const Interiors = () => {
+  return (
+    <div>
+      <h2>Interiors</h2>
+    </div>
+  )
+};
+
+export default Interiors;
