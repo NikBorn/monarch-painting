e371db33da94d30ef4864e7e74768eba97791fa0
Nik Bornemeier <NikolausRB@gmail.com>
2018-11-26 10:52:29 -0700

    add router and dependencies, create HomePage

---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <React.Fragment>
+      <h1>MONARCH PAINTING</h1>
+      <h2>Your Home Painting Friends</h2>
+    </React.Fragment>
+  )
+};
+
+export default HomePage;
