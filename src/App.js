9cb84a427cb3c7d438e11c61cd311dbcdd20e7dd
Nik Bornemeier <NikolausRB@gmail.com>
2018-11-29 09:58:42 -0700

    add Contact, About, and Services Components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import HomePage from './Components/HomePage/HomePage.js';
 import Exteriors from './Components/Exteriors/Exteriors.js';
 import Interiors from './Components/Interiors/Interiors.js';
 import TabBar from './Components/TabBar/TabBar.js';
+import About from './Components/About/About.js';
+import Services from './Components/Services/Services.js';
+import Contact from './Components/Contact/Contact.js';
+
 import { Route } from 'react-router';
 
 class App extends Component {
@@ -26,6 +30,24 @@ class App extends Component {
         <Route exact path='/Exteriors'
           render={() =>
             <Exteriors />
+          }
+        />
+
+        <Route exact path='/About'
+          render={() =>
+            <About />
+          }
+        />
+
+        <Route exact path='/Services'
+          render={() =>
+            <Services />
+          }
+        />
+
+        <Route exact path='/Contact'
+          render={() =>
+            <Contact />
           }
         />
 
--- src/Components/TabBar/TabBar.js
+++ src/Components/TabBar/TabBar.js
@@ -8,6 +8,7 @@ class TabBar extends Component {
   render() {
     return(
       <div className='tab-bar'>
+        <a href='/' className='tab'>Home</a>
         <a href='/about'className='tab'>About</a>
         <a href='/services' className='tab'>Services</a>
         <a href='/contact' className='tab'>Contact</a>
