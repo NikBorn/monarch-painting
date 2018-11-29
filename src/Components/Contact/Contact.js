70954efe0af32b1cba94efc62e9afe960d93c8ed
Nik Bornemeier <NikolausRB@gmail.com>
2018-11-29 10:48:12 -0700

    add some content to Contact Component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,6 +4,9 @@ const Contact = () => {
   return (
     <div>
       Contact
+      <p>Please contact us today so we can further explain how we can help you with all of your residential painting and staining needs.</p>
+      <a href='mailto:NikolausRB@gmail.com'>Email Monarch</a>
+      <a href='tel:1-720-883-1603'>Call Monarch</a>
     </div>
   )
 };
